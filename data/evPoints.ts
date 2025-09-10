2b8230a964e23d30bf9edbb96473f2bd1de9907f
kamrangul87 <kamrangul87@gmail.com>
2025-09-10 08:48:48 +0100

    Create evPoints.ts

---
+++ data/evPoints.ts
@@ -0,0 +1,7 @@
+const points = [
+  { lat: 51.5074, lng: -0.1278, value: 8 },   // London
+  { lat: 51.4545, lng: -2.5879, value: 3 },   // Bristol
+  { lat: 52.4862, lng: -1.8904, value: 5 },   // Birmingham
+  { lat: 53.4808, lng: -2.2426, value: 7 },   // Manchester
+];
+export default points;
