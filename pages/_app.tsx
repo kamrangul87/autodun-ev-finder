a426a143f295331b1cd42337a3ba44ff5e69ee14
kamrangul87 <kamrangul87@gmail.com>
2025-09-10 09:51:36 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
+// pages/_app.tsx
 import type { AppProps } from 'next/app';
-import 'leaflet/dist/leaflet.css';
+import '../app/globals.css';       // Tailwind/global styles
+import 'leaflet/dist/leaflet.css'; // Leaflet styles
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
