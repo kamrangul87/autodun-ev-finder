57082bcb143e69bdb416a1d51f7c042141c887d4
kamrangul87 <kamrangul87@gmail.com>
2025-09-15 07:48:49 +0100

    Create pwa.d.ts

---
+++ types/pwa.d.ts
@@ -0,0 +1,9 @@
+// types/pwa.d.ts
+export {};
+declare global {
+  interface BeforeInstallPromptEvent extends Event {
+    readonly platforms: string[];
+    prompt(): Promise<void>;
+    userChoice: Promise<{ outcome: 'accepted' | 'dismissed'; platform: string }>;
+  }
+}
