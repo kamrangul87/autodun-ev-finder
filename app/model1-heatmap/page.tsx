ff3b9d17683deec8a14b13c0dc89bdb37ec367a9
kamrangul87 <kamrangul87@gmail.com>
2025-09-20 11:31:46 +0100

    Update page.tsx

--- app/model1-heatmap/page.tsx
+++ app/model1-heatmap/page.tsx
@@ -1,10 +1,10 @@
 export const dynamic = 'force-dynamic';
 export const revalidate = false;
 
-import dynamic from 'next/dynamic';
+import nextDynamic from 'next/dynamic';
 
 // Render the client-only map without SSR to avoid "window is not defined"
-const Client = dynamic(() => import('./Client'), { ssr: false });
+const Client = nextDynamic(() => import('./Client'), { ssr: false });
 
 export default function Page() {
   return <Client />;
