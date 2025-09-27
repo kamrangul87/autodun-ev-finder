db6951094734ee12cd9415ce4019a2caaee1523c
kamrangul87 <kamrangul87@gmail.com>
2025-09-27 11:02:09 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,3 @@
-"use client";
-export const viewport = { themeColor: '#0b1220' };
-import Model1HeatmapPage from './model1-heatmap/page';
-
-export default function HomePage() {
-  return <Model1HeatmapPage />;
-}
-
-"use client";
 export const viewport = { themeColor: '#0b1220' };
 import Model1HeatmapPage from './model1-heatmap/page';
 
