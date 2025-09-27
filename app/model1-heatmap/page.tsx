f2b032ddccaf50de3fff5db8094c1efc59dbd881
kamrangul87 <kamrangul87@gmail.com>
2025-09-27 11:39:12 +0100

    Update page.tsx

--- app/model1-heatmap/page.tsx
+++ app/model1-heatmap/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
+export const dynamic = 'force-dynamic'; // disable prerender/SSG for this page
 
 import dynamicImport from "next/dynamic";
 import { useState } from "react";
