7867f66879f12bb76fe644fd4fb56950c091387f
kamrangul87 <kamrangul87@gmail.com>
2025-09-27 11:34:23 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+export const dynamic = 'force-dynamic';   // don't prerender/SSG the root either
 export const viewport = { themeColor: '#0b1220' };
-import Model1HeatmapPage from './model1-heatmap/page';
+
+import dynamic from "next/dynamic";
+const Model1HeatmapPage = dynamic(() => import("./model1-heatmap/page"), { ssr: false });
 
 export default function HomePage() {
   return <Model1HeatmapPage />;
