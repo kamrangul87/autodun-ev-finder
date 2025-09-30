e44243667b81686fbd306310d21982485a9d4753
kamrangul87 <kamrangul87@gmail.com>
2025-09-30 11:56:07 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,11 @@ export const viewport: Viewport = {
   themeColor: "#38bdf8",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className="min-h-screen flex flex-col">
