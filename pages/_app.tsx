569fa486ffc56e99b1e935fe716cc1eba7625aab
kamrangul87 <kamrangul87@gmail.com>
2025-10-04 08:37:40 +0000

    fix: ensure leaflet deps and remove global css import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app';
 import '../app/globals.css';       // Tailwind/global styles
-import 'leaflet/dist/leaflet.css'; // Leaflet styles
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
