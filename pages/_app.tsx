4a8afc2a15abac22864e3cbb8d9408e152f358c0
kamrangul87 <kamrangul87@gmail.com>
2025-10-09 07:19:48 +0000

    fix: use styles/globals.css in pages/_app; remove App Router CSS reference

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app';
-import '../app/globals.css';       // Tailwind/global styles
+import '../styles/globals.css';       // Tailwind/global styles
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
