855af0293a8b759f8dcb459328132853a213ff19
kamrangul87 <kamrangul87@gmail.com>
2025-10-21 11:47:10 +0100

    Update EnhancedMapV2.jsx

--- components/EnhancedMapV2.jsx
+++ components/EnhancedMapV2.jsx
@@ -14,7 +14,7 @@ import MarkerClusterGroup from "react-leaflet-cluster";
 import L from "leaflet";
 
 
-import { StationDrawer } from "./StationDrawer";
+import StationDrawer, { StationDrawer as NamedStationDrawer } from './StationDrawer.jsx';
 import { LocateMeButton } from "./LocateMeButton.tsx";
 import { getCached, setCache } from "../lib/api-cache";
 import { telemetry } from "../utils/telemetry.ts";
