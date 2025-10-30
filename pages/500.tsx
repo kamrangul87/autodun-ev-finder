6304304f447dba548db083558db79221b0cc99da
kamrangul87 <kamrangul87@gmail.com>
2025-10-30 07:54:14 +0000

    Create 500.tsx

---
+++ pages/500.tsx
@@ -0,0 +1,9 @@
+// pages/500.tsx
+export default function ServerError() {
+  return (
+    <main style={{ maxWidth: 720, margin: '60px auto', padding: '0 16px' }}>
+      <h1>Something went wrong</h1>
+      <p>Please try again in a moment. <a href="/">Go home</a>.</p>
+    </main>
+  );
+}
