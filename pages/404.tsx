6988d62f6957dc67264f0b47f1fa143c1ff2985c
kamrangul87 <kamrangul87@gmail.com>
2025-10-30 07:53:46 +0000

    Create 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+// pages/404.tsx
+export default function NotFound() {
+  return (
+    <main style={{ maxWidth: 720, margin: '60px auto', padding: '0 16px' }}>
+      <h1>Not found</h1>
+      <p>We couldn’t find that page. <a href="/">Go home</a>.</p>
+    </main>
+  );
+}
